6432a33bef49782953eb5541c43606a57fd296d0
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-16 21:42:55 +0900

    자격증 내용 수정

--- back/src/services/certificateService.js
+++ back/src/services/certificateService.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 
 class certificateAuthService {
     static async addCertificate({title, description,when_date}){
+        // 자격증 등록
         const newCertificate = {title, description, when_date};
 
         const createNewCertificate = await Certificate.create({newCertificate});
@@ -12,9 +13,33 @@ class certificateAuthService {
 
         return createdNewCertificate;
     }
+    // 자격증 목록 불러오기
     static async getCertificates({}){
         const  certificates = await Certificate.findAll();
         return certificates;
+    }
+    // 자격증 내용 수정
+    static async setCertificate({toUpdate}){
+        const certificate = await Certificate.findAll();
+        
+        if(toUpdate.title) {
+            const fieldToUpdate = "title";
+            const newValue = toUpdate.title;
+            certificate = await Certificate.update({fieldToUpdate,newValue});
+        }
+
+        if(toUpdate.description) {
+            const fieldToUpdate = "description";
+            const newValue = toUpdate.description;
+            certificate = await Certificate.update({fieldToUpdate,newValue});
+        }
+
+        if (toUpdate.when_date) {
+            const fieldToUpdate = "when_date";
+            const newValue = toUpdate.when_date;
+            certificate = await Certificate.update({fieldToUpdate,newValue});
+        }
+        return certificate;
     }
 }
 export {certificateAuthService};
