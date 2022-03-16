cace3362fb0e221c4035fd92a94b0f5063610b23
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-16 16:45:15 +0900

    자격증 수정 함수 생성

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -1,9 +1,18 @@
 import {CertificateModel} from '../schemas/certificate';
+import { UserModel } from '../schemas/user';
 
 class Certificate {
     static async create({newCertificate}){
         const createCertificate = await CertificateModel.create(newCertificate);
         return createCertificate;
+    }
+    static async update({user_id,fieldToUpdate, newValue}){
+        const filter = {id: user_id};
+        const update = {[fieldToUpdate]: newValue};
+        const option = {returnOriginal: false};
+
+        const updateUser = await UserModel.findOneAndUpdate(user_id, filter, update);
+        return updateUser;
     }
     
 
