62839d5833e805071bb71164c1eb001b7e65d025
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-16 22:13:24 +0900

    불필요한 부분 삭제 및 id 유니크 값 사용

--- back/src/services/certificateService.js
+++ back/src/services/certificateService.js
@@ -1,12 +1,11 @@
-import {User, Certificate} from '../db';
-import bcrypt from 'bcrypt';
+import {Certificate} from '../db';
 import {v4 as uuidv4} from 'uuid';
-import jwt from 'jsonwebtoken';
 
 class certificateAuthService {
-    static async addCertificate({title, description,when_date}){
+    static async addCertificate({userId, title, description,when_date}){
         // 자격증 등록
-        const newCertificate = {title, description, when_date};
+        const id = uuidv4();
+        const newCertificate = {id, userId, title, description, when_date};
 
         const createNewCertificate = await Certificate.create({newCertificate});
         createNewCertificate.errorMessage = null;
