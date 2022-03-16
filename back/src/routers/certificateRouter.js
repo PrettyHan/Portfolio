7058e9b6cb462f56507b03d6530eb4072c491c46
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-16 21:11:02 +0900

    라우터 주석 작성

--- back/src/routers/certificateRouter.js
+++ back/src/routers/certificateRouter.js
@@ -16,10 +16,12 @@ certificateAuthRouter.post('/certificate/create', async function(req,res,next){
             );
 
         }
+        /// req에서 데이터 가져오기
         const title = req.body.title;
         const description = req.body.description;
         const when_date = req.body.when_date;
 
+        // 위 데이터를 자격증 db에 추가하기
         const newCertificate = await certificateAuthService.addCertificate({
             title,
             description,
