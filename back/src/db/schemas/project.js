18b7d3e2048f844c453cfed3cfcd0ba1d166fbc1
jjs0211 <jjs_0211_@naver.com>
2022-03-16 22:25:42 +0900

    MongoDB 연결 & Project MVP Schema 작성

---
+++ back/src/db/schemas/project.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const projectSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    period: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = model("User", UserSchema);
+
+export { UserModel };
