46f610f3c873a7e1de6ec2c9945846591d361448
PrettyHan <imbel9830@gmail.com>
2022-03-16 10:47:45 +0900

    DB Add service 추가

---
+++ back/src/db/models/Education.js
@@ -0,0 +1,31 @@
+import { EducationModel } from "../schemas/education";
+
+class Education {
+   // 입력 받은 education 정보를 생성 
+  static async create({ newEducation }) {
+    const createdNewEducation = await EducationModel.create(newEducation);
+    return createdNewEducation;
+  }
+
+  static async findBySchool({ school }) {
+    // 입력 받은 school을 기준으로 db에서 검색하여 추출
+    const findNewEducation = await EducationModel.findOne({school})
+    return findNewEducation
+  }
+
+  static async findByMajor ({major}) {
+    // 입력 받은 major를 기준으로 db에서 검색하여 추출
+    const findNewEducation = await EducationModel.findOne({major})
+    return findNewEducation
+  }
+
+  static async findByStatus ({user_status}) {
+    // 입력 받은 status를 기준으로 db에서 검색하여 추출
+    const findNewEducation = await EducationModel.findOne({user_status})
+    return findNewEducation
+  }
+
+
+}
+
+export { Education };
