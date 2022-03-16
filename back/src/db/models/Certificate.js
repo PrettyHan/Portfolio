c7206cc647b6d5f86c4bbdb33f8da6e5408f968a
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-16 21:09:14 +0900

    자격증 수정 (...ing)

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -6,6 +6,18 @@ class Certificate {
         const createCertificate = await CertificateModel.create(newCertificate);
         return createCertificate;
     }
+    static async findByEmail({email}){
+        const user = await UserModel.findByEmail({ email });
+        return user;
+    }
+    static async findById({user_id}) {
+        const user = await UserModel.findOne({id : user_id});
+        return user;
+    }
+    static async findAll(){
+        const users = await UserModel.find({});
+        return users;
+    }
     static async update({user_id,fieldToUpdate, newValue}){
         const filter = {id: user_id};
         const update = {[fieldToUpdate]: newValue};
