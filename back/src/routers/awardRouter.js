3169e2bc9ed23064a23ecb2726e4dc79b260b8b1
jjs0211 <jjs_0211_@naver.com>
2022-03-16 12:18:30 +0900

    주석 추가

--- back/src/routers/awardRouter.js
+++ back/src/routers/awardRouter.js
@@ -4,7 +4,7 @@ import { login_required } from "../middlewares/login_required";
 import { AwardService } from "../services/awardService";
 
 const awardRouter = Router();
-// 로그인 체크 여부 확인
+// 로그인 체크 여부 확인(postman 사용할 때는 있으면 로그인이 필요합니다 뜸)
 // awardRouter.use(login_required);
 
 awardRouter.post("/award/create", async function (req, res, next) {
