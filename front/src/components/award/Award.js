ed0925ab28433c63990da569c35bb144d3a09097
jjs0211 <jjs_0211_@naver.com>
2022-03-17 16:10:42 +0900

    Award MVP 연결 완료

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,49 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Card, Button, Row, Col } from "react-bootstrap";
-import * as Api from "../../api";
-import Award from "./Award";
-import AwardAddForm from "./AwardAddForm";
+import React, { useState } from "react";
+import AwardEditForm from "./AwardEditForm";
+import AwardCard from "./AwardCard";
 
-function Awards({ portfolioOwnerId, isEditable }) {
-  //useState로 awards 상태를 생성함.
-  const [awards, setAwards] = useState([]);
-  //useState로 isAdding 상태를 생성함.
-  const [isAdding, setIsAdding] = useState(false);
-
-  useEffect(() => {
-    // "awardlist/유저id"로 GET 요청하고, response의 data로 awards를 세팅함.
-    Api.get("awardlist", portfolioOwnerId).then((res) => setAwards(res.data));
-  }, [portfolioOwnerId]);
-
+function Award({ award, setAwards, isEditable }) {
+  //useState로 isEditing 상태를 생성
+  const [isEditing, setIsEditing] = useState(false);
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>수상이력</Card.Title>
-        {awards.map((award) => (
-          <Award
-            key={award.id}
-            award={award}
-            setAwards={setAwards}
-            isEditable={isEditable}
-          />
-        ))}
-        {isEditable && (
-          <Row className="mt-3 text-center mb-4">
-            <Col sm={{ span: 20 }}>
-              <Button onClick={() => setIsAdding(true)}>+</Button>
-            </Col>
-          </Row>
-        )}
-        {isAdding && (
-          <AwardAddForm
-            portfolioOwnerId={portfolioOwnerId}
-            setAwards={setAwards}
-            setIsAdding={setIsAdding}
-          />
-        )}
-      </Card.Body>
-    </Card>
+    <>
+      {isEditing ? (
+        <AwardEditForm
+          currentAward={award}
+          setAwards={setAwards}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <AwardCard
+          award={award}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
   );
 }
 
-export default Awards;
+export default Award;
