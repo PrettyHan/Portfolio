30b86ddc15ca2467dfd2b317a366822fda418520
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-18 14:12:40 +0900

    주석 추가 및 삭제 기능 주석 처리

--- back/src/db/models/Certificate.js
+++ back/src/db/models/Certificate.js
@@ -1,19 +1,22 @@
 import {CertificateModel} from '../schemas/certificate';
 
 class Certificate {
+    // 새로운 자격증 등록
     static async create({newCertificate}){
         const createdNewCertificate = await CertificateModel.create(newCertificate);
         return createdNewCertificate;
     }
-
+    // 자격증의 Id 기준 검색
     static async findById({certificateId}) {
         const certificate = await CertificateModel.findOne({id : certificateId});
         return certificate;
     }
+    // 사용자의 Id 기준 검색
     static async findByUserId({ userId }){
         const certificates = await CertificateModel.find({userId});
         return certificates;
     }
+    // 자격증 내용 수정
     static async update({certificateId,fieldToUpdate, newValue}){
         const filter = {id: certificateId};
         const update = {[fieldToUpdate]: newValue};
@@ -22,10 +25,11 @@ class Certificate {
         const updateCertificate = await CertificateModel.findOneAndUpdate( filter, update, option);
         return updateCertificate;
     }
-    static async delete({createdCertificate}){
-        const deletedCertificate = await CertificateModel.delete({createdCertificate});
-        return deletedCertificate;
-    }
+    // 자격증 삭제
+    // static async delete({createdCertificate}){
+    //     const deletedCertificate = await CertificateModel.delete({createdCertificate});
+    //     return deletedCertificate;
+    // }
 
 }
 
