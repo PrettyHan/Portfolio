24cd7e418e1c747040c5901c7eb1012f90ac1f5e
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-19 15:03:01 +0900

    오타 수정

--- back/src/app.js
+++ back/src/app.js
@@ -28,7 +28,7 @@ app.use(userAuthRouter);
 app.use(educationRouter);
 app.use(projectRouter);
 app.use(awardRouter);
-app.user(certificateAuthRouter);
+app.use(certificateAuthRouter);
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
 
