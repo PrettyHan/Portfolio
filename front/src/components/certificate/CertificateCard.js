f419201e03da4847af8a751644bce92418947201
jjs0211 <jjs_0211_@naver.com>
2022-03-19 15:41:51 +0900

    1차 구현 완료

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,6 +1,9 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
 
 function CertificateCard({ certificate, isEditable, setIsEditing }) {
+  const toStringwhen_date = ''+ certificate.when_date;
+  const when_date = toStringwhen_date.substring(0,10);
+
   return (
     <Card.Text>
       <Row className="align-items-center">
@@ -9,7 +12,7 @@ function CertificateCard({ certificate, isEditable, setIsEditing }) {
           <br />
           <span className="text-muted">{certificate.description}</span>
           <br />
-          <span className="text-muted">{certificate.when_date}</span>
+          <span className="text-muted">{when_date}</span>
         </Col>
         {isEditable && (
           <Col xs lg="1">
