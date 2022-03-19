b021a591c5e1e7232f7d39cb22940da1201073b6
[4기-2반] 장소영 <soy5360@naver.com>
2022-03-19 14:54:36 +0900

    오타 수정

--- back/src/routers/certificateRouter.js
+++ back/src/routers/certificateRouter.js
@@ -1,62 +1,114 @@
-import {Certificate} from '../db';
-import {v4 as uuidv4} from 'uuid';
+import is from "@sindresorhus/is";
+import {Router} from 'express';
+import {login_required} from '../middlewares/login_required';
+import { certificateAuthService } from "../services/certificateService";
 
-class certificateAuthService {
-    // 자격증 추가
-    static async addCertificate({userId, title, description,when_date}){
-        // 자격증 id의 유니크 값
-        const id = uuidv4();
-        const newCertificate = {id, userId, title, description, when_date};
+const certificateAuthRouter = Router();
+
+// certificateAuthRouter.use(login_required);
+
+// 자격증 추가 Post 요청
+certificateAuthRouter.post('/certificate/create', async function(req,res,next){
+    try {
+        // 사용자의 새로운 자격증 등록
+        if(is.emptyObject(req.body)){
+            throw new Error(
+                `headers의 Content-Type을 application/json으로 설정해주세요`
+            );
+
+        }
+        /// req에서 데이터 가져오기
+        const userId = req.body.userId;
+        const title = req.body.title;
+        const description = req.body.description;
+        const when_date = req.body.when_date;
+
+        // 위 데이터를 자격증 db에 추가하기
+        const newCertificate = await certificateAuthService.addCertificate({
+            userId : userId,
+            title : title,
+            description : description,
+            when_date : when_date,
+        });
         
-        const createdNewCertificate = await Certificate.create({newCertificate});
-        createdNewCertificate.errorMessage=null;
-        return createdNewCertificate;
+        if (newCertificate.errorMessage) {
+            throw new Error(newCertificate.errorMessage);
+        }
+        res.status(201).json(newCertificate);
+        
     }
+    catch(error) {
+        next(error);
+    }
+});
 
-    // 자격증 내용 불러오기
-    static async getCertificates({certificateId}){
-        const  certificates = await Certificate.findById({certificateId})
-        return certificates;
-    }
+// 사용자가 가지고 있는 자격증 내용 get 요청 
+certificateAuthRouter.get('/certificates/:id',async function(req, res, next){
+    try{
+        // 자격증 Id 기준 불러오기
+        const certificateId = req.params.id;
+        const currentCertificateInfo = await certificateAuthService.getCertificates({certificateId});
+        res.status(200).send(currentCertificateInfo);
 
-    // 자격증 내용 수정
-    static async setCertificate({certificateId,toUpdate}){
-        let certificate = await Certificate.findById({certificateId});
-        // 자격증 보유 확인
-        if (!certificate) {
-            const errorMessage = `해당 id를 가진 자격증 이력은 없습니다. 다시 한 번 확인해주세요.`;
-            return {errorMessage};
+        if (currentCertificateInfo.errorMessage){
+            throw new Error(currentCertificateInfo.errorMessage);
         }
-        // 자격증 이름 수정
-        if(toUpdate.title) {
-            const fieldToUpdate = "title";
-            const newValue = toUpdate.title;
-            certificate = await Certificate.update({certificateId,fieldToUpdate,newValue});
-        }
-        // 자격증 내용 수정
-        if(toUpdate.description) {
-            const fieldToUpdate = "description";
-            const newValue = toUpdate.description;
-            certificate = await Certificate.update({certificateId,fieldToUpdate,newValue});
-        }
-        // 자격증 날짜 수정
-        if (toUpdate.when_date) {
-            const fieldToUpdate = "when_date";
-            const newValue = toUpdate.when_date;
-            certificate = await Certificate.update({certificateId,fieldToUpdate,newValue});
-        }
-        return certificate;
+        res.status(200).send(currentCertificateInfo);
+    }catch (error){
+        next(error);
     }
+})
 
-    // 보유한 자격증 목록 불러오기
-    static async getCertificateList({userId}){
-        const certificates = await Certificate.findByUserId({userId});
-        return certificates;
+// 사용자의 자격증 수정 put 요청
+certificateAuthRouter.put('/certificates/:id',
+    async function (req,res,next){
+        try {
+            //URI로부터 사용자 id 추출
+            const certificateId = req.params.id;
+            // body data로부터 업데이트할 사용자 정보를 추출
+            const title = req.body.title ?? null;
+            const description = req.body.description ?? null;
+            const when_date = req.body.when_date ?? null;
+
+            const toUpdate = {title, description, when_date};
+            // 해당 사용자 아이디로 사용자 정보를 db에서 찾아 업데이트. 업데이트 내용 없을 시 생략
+            const updateCertificate = await certificateAuthService.setCertificate({certificateId,toUpdate});
+
+            if(updateCertificate.errorMessage) {
+                throw new Error(updateCertificate.errorMessage);
+            }
+            
+            res.status(200).json(updateCertificate);
+        } catch(error){
+            next(error);
+        }
     }
-    // 자격증 삭제
-    // static async deleteCertificateList({userId}){
-    //     const deletedCertificates = await Certificate.delete({userId});
-    //     return deletedCertificates;
-    // }
-}
-export {certificateAuthService};
+)
+
+// 사용자가 보유한 자격증 목록 불러오기 get 요청
+certificateAuthRouter.get('/certificatelist/:userId',
+    async function (req,res,next){
+        try{
+            const userId = req.params.userId;
+            const certificateList = await certificateAuthService.getCertificateList({userId});
+            res.status(200).send(certificateList);
+        } catch(error){
+            next(error);
+        }
+    }
+)
+
+// 보유한 자격증 삭제 delete 요청 자격증Id 기준으로 삭제
+// certificateAuthRouter.delete('certificatelist/:userId',
+//     async function (req, res, next){
+//         try {
+//             const userId = req.params.userId;
+//             const deletedCertificateList = await certificateAuthService.deleteCertificateList({userId});
+//             res.status(200).send(deletedCertificateList);
+//         }catch(error){
+//             next(error);
+//         }
+//     }
+// )
+
+export {certificateAuthRouter};
