d91f5a287ece5c722d4b8fdc50856ad1aef2ff85
[4기-2반] 이자원 <ljw1491@u.sogang.ac.kr>
2022-03-25 22:54:36 +0900

    버튼간격 조정, 문구 수정

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -11,7 +11,7 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork, isClick }) {
   // 탈퇴기능
   const deleteUser = async (id) => {
     try {
-      if (window.confirm('정말로 탈퇴하시겠습니까?')) {
+      if (window.confirm('정말로 탈퇴하시겠습니까? 😢')) {
         await Api.delete(`user/${id}`);
         sessionStorage.clear(); // 모든 데이터 삭제
         alert('탈퇴가 완료되었습니다. 이용해주셔서 감사합니다.');
@@ -46,27 +46,23 @@ function UserCard({ user, setIsEditing, isEditable, isNetwork, isClick }) {
            <div>{user?.description}</div>
             <Row className="mt-3 text-center text-info">
               <Col sm={{ span: 20 }}>
-              <Button
-               mb="10"
-               style={{
-                border:"none",
-                backgroundColor:"#339AF0",
-                color: "#fffff"
-                }} 
-                size="sm"
-                onClick={() => setIsEditing(true)}
+                <Button
+                  mb="10"
+                  variant="outline-info"
+                  size="sm"
+                  onClick={() => setIsEditing(true)}
                 >
                   편집
                 </Button>
+                &nbsp;&nbsp;&nbsp;&nbsp;
                 <Button
                   className="ml-3 mr-3"
                   variant="outline-danger"
                   size="sm"
                   onClick={() => deleteUser(user.id)}
                 >
-                  탈퇴
+                  회원탈퇴
                 </Button>
-
               </Col>
             </Row>
           </Col>
